85d48addf408ca215ee9bf988cc49f20addd4509
lucasbeckerok <campus.becker@gmail.com>
2022-03-01 16:50:37 -0300

    clase 4

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import Footer from "./Footer"
 
 
 function App() {
+
+    const edad = 28;
+    const nombre = ["Juan", "Pedro"];
+
     return (
         <>
             <Header main="main-header"/>
-            <Main main="main-body" />
+            <Main main="main-body" className={edad} className={nombre} initial={0}/>
             <Footer footer="main-footer"/>
         </>
     )
--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,31 @@
-const Main = () => {
+import {useState} from 'react'
+
+const Main = (props) => {
+
+    // const resultado = useState(); // Siempre retorna un array con dos elementos
+
+    // let contador = 0
+
+    const [contador, setContador] = useState(props.initial);
+
+    const handleclick = () => {
+        setContador(contador + 1)
+    }
+    const restar = () => {
+        setContador(contador - 1)
+    }
+
+    const resetear = () => {
+        setContador(0)
+    }
+
     return (
     <main>
         <div id="presentacion">
-            <h1>bienvenido a la tienda e-commerce</h1>
-            <h2>ofertas diarias</h2>
-            <p>Descripcion de la Aplicacion tienda ecommerce</p>
+            <h1>bienvenido {props.nombre} {props.apellido} a la tienda e-commerce</h1>
+            <h2>Mi contador actual: {contador}</h2>
+            <button onClick={handleclick}>Aumentar</button>
+            <button onClick={restar}>restar</button>
         </div>
     </main>
     )
