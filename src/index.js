1139e765426da81616eb7dc38321e58d41db3305
lucasbeckerok <campus.becker@gmail.com>
2022-03-02 19:54:47 -0300

    se instalo bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./App"
-import "./scss/App.scss"
+import App from "./App";
+import "./scss/App.scss";
 
 ReactDOM.render(<App/>,document.getElementById('root'))
