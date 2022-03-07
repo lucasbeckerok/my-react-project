5bf5a1f55cd469c84401354eddf42898a93b7ec1
lucasbeckerok <campus.becker@gmail.com>
2022-03-06 23:16:05 -0300

    entrega desafio contador

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -3,6 +3,7 @@ import ItemList from './ItemList';
 const ItemListContainer = (props) => {
     return (
       <>
+      <h2>Productos</h2>
       <div id="itemStoreContainer">
         <ItemList/>
       </div>
