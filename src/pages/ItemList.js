8f02ac0a34ac820a16a361d1a9b9706400ff9773
lucasbeckerok <campus.becker@gmail.com>
2022-03-12 19:00:33 -0300

    revision entrega desafio promise, asincronia y map

--- src/pages/ItemList.js
+++ src/pages/ItemList.js
@@ -1,32 +1,12 @@
 import Item from "./Item"
-import { useState, useEffect } from "react"
 
-let productsInitial = [
-    { name: "Papas Fritas", descripcion:"250 gr.", img: "./img/papas.jpg", price: 150, stock: 5 },
-    { name: "Gaseosa", descripcion: "250 ml.", img: "./img/gaseosa.jpg", price: 200, stock: 8 },
-    { name: "Chocolate", descripcion: "400 gr.", img: "./img/chocolate.jpg", price: 300, stock: 5 },
-    { name: "Cigarros", descripcion: "pack de 20 cigarrillos", img: "./img/cigarro.jpg", price: 200, stock: 10 }
-]
-
-const ItemList = () => {
-    
-    const [loading, setLoading] = useState(true);
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setProducts(productsInitial);
-            setLoading(false);
-        }, 3000)}, []);
+const ItemList = (props) => {
 
     return (
         <>
-        
-            <h2>{loading && "cargando...."}</h2>
-            {products.map((product, indice) => {
-                return <Item key={indice} name={product.name} descripcion={product.descripcion} price={product.price} stock={product.stock} img={product.img}/>
+            {props.products.map((product, id) => {
+                return <Item key={product.id} name={product.name} price={product.price} stock={product.stock} img={product.img} descripcion={product.descripcion} />
             })}
-        
         </>
     )
 }
--- src/pages/ItemListContainer.js
+++ src/pages/ItemListContainer.js
@@ -1,14 +1,35 @@
-import ItemList from './ItemList';
+import ItemList from "./ItemList"
+import { useState, useEffect } from "react"
+
+let productsInitial = [
+    { id: 1, name: "Papas Fritas", descripcion:"250 gr.", img: "./img/papas.jpg", price: 150, stock: 5 },
+    { id: 2, name: "Gaseosa", descripcion: "250 ml.", img: "./img/gaseosa.jpg", price: 200, stock: 8 },
+    { id: 3, name: "Chocolate", descripcion: "400 gr.", img: "./img/chocolate.jpg", price: 300, stock: 5 },
+    { id: 4, name: "Cigarros", descripcion: "pack de 20 cigarrillos", img: "./img/cigarro.jpg", price: 200, stock: 10 }
+]
 
 const ItemListContainer = () => {
+    
+    const [loading, setLoading] = useState(true);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+
+      setTimeout(() => {
+
+        setProducts(productsInitial);
+
+        setLoading(false);
+
+        }, 2000)
+        
+      })
+
     return (
-      <>
-      <h2>Productos</h2>
-      <div id="itemStoreContainer" className="cards">
-        <ItemList/>
-      </div>
-      </>
-  )
+        <>
+            {loading ? <h2>Cargando Productos... Por favor aguarde</h2> : <div className="cards"><ItemList products={products} /></div>}
+        </>
+    )
 }
 
 export default ItemListContainer;
