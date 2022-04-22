823d3f418bdd93d07bfa33cc164434dec893d5c1
lucasbeckerok <campus.becker@gmail.com>
2022-04-22 00:44:21 -0300

    leves cambios esteticos

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,16 +1,11 @@
-
 import React from "react";
-import DarkToggle from "../widgets/DarkToggle";
 
 const Footer = () => {
   return(
         <footer className="footer">
-            <h3>Contacto</h3>
             <ul>
               <li><a href="#" class="fa fa-facebook"></a>  <a href="#" class="fa fa-instagram"></a></li>
-              <li>Bariloche, Argentina</li>
-              <li>Email: hola@hannokiosco.com.ar</li>
-              <li>Todos Los Derechos Reservados 2022</li>
+              <li>Todos Los Derechos Reservados 2022 - Lucas Becker</li>
             </ul>
         </footer>
 )}
